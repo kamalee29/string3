bb974bd671ae97116954e1b5222fc90a76c804bc
kamalee29 <kamalee.selvii24@gmail.com>
2016-07-18 02:29:55 -0700

    Add files via upload

---
+++ great.java
@@ -0,0 +1,8 @@
+
+   if(a>b&&a>c)
+            System.out.println("a is greater");
+        else if(b>c)
+            System.out.println("b is greater");
+        else
+            System.out.println("c is greater");
+        
