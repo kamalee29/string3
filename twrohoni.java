2561a8381a43b8c61b428df9b489a0de94520249
kamalee29 <kamalee.selvii24@gmail.com>
2016-08-07 21:56:55 -0700

    Add files via upload

---
+++ twrohoni.java
@@ -0,0 +1,9 @@
+if (topN == 1){
+         System.out.println("Disk 1 from "
+         + from + " to " + to);
+      }else {
+         doTowers(topN - 1, from, to, inter);
+         System.out.println("Disk "
+         + topN + " from " + from + " to " + to);
+         doTowers(topN - 1, inter, from, to);
+      }
